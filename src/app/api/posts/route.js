0bdcfc02e32cd2a87d920d819a78489a80ae8ff7
nabiursiddique <nabiursiddique01@gmail.com>
2023-08-30 21:45:59 +0600

    secret keys are removed from github

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -6,7 +6,7 @@ export const GET = async (request) => {
     try {
         await connect()
         const posts = await Post.find()
-        return new NextResponse(posts, { status: 200 })
+        return new NextResponse(JSON.stringify(posts), { status: 200 })
     }
     catch (err) {
         return new NextResponse("Database Error", { status: 500 });
